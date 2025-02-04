541a263ffe7ed89908eaca50640e1d35d4356768
Ferry Ayunda <ayunda.fa0604@gmail.com>
2025-02-04 19:03:14 +0700

    Update share.js

--- share.js
+++ share.js
@@ -1,22 +1,28 @@
-async function shareCanvas() {
-  const canvasElement = document.getElementById('preview');
-  const dataUrl = canvasElement.toDataURL();
-  const blob = await (await fetch(dataUrl)).blob();
-  const filesArray = [
-    new File(
-      [blob],
-      'image.png',
-      {
-        type: blob.type,
-        lastModified: new Date().getTime()
-      }
-    )
-  ];
-  const shareData = {
-    files: filesArray,
-    title: "Twibbon Aplikasi",
-    text: "Twibbon adalah cara sederhana untuk mengekspresikan dukungan dan partisipasi kita dalam suatu acara atau gerakan.",
-    url: "https://xtcodes.github.io/twibbon/",
-  };
-  navigator.share(shareData);
+document.getElementById("shared").addEventListener("click", () => {
+alert("Bagikan Twibbon ini ke media sosial Anda?");
+
+const dataUrl = preview.toDataURL(); 
+fetch(dataUrl)
+.then(res => res.blob())
+.then(blob => {
+//console.log(blob)
+const filesArray = [new File([blob], 'image.png', { type: blob.type, lastModified: new Date().getTime() })];
+console.log(filesArray);
+const shareData = {
+title: "FREE AND SIMPLE PFP GENERATOR",
+text: "FREE AND SIMPLE PFP GENERATOR",
+url: "https://badut.pages.dev/",
+files: filesArray
+};
+console.log(shareData);
+if (navigator.share) {
+navigator.share(
+shareData
+)
+.then(() => alert("Terima kasih telah berbagi.")) 
+.catch(error => alert("Bagikan dibatalkan!", error)); 
+} else {
+alert('navigator.share not available'); 
 }
+})
+});
